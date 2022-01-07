36acd0e9a25128be98b6107a084ac4b0c71c3a37
Martin Erskine <martin.erskine@gmail.com>
2022-01-07 10:04:46 +0000

    Extract Classes

---
+++ LinkedList/LinkedNode.cs
@@ -0,0 +1,12 @@
+﻿namespace LinkedList
+{
+    public class LinkedNode<T>
+    {
+        public LinkedNode<T> next;
+        public T Data;
+        public LinkedNode(T Data)
+        {
+            this.Data = Data;
+        }
+    }
+}
